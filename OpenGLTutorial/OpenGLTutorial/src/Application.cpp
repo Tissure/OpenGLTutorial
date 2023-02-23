8276020b330b7366c0f19325bb070e34e063281e
Kyle <kyle.ng7512@gmail.com>
2023-02-23 15:30:47 -0800

    Complete part 12 - Vertex Arrays

--- OpenGLTutorial/OpenGLTutorial/src/Application.cpp
+++ OpenGLTutorial/OpenGLTutorial/src/Application.cpp
@@ -111,6 +111,10 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -146,15 +150,20 @@ int main(void)
         2,3,0
     };
 
+    unsigned int vao;
+    GLCall(glGenVertexArrays(1, &vao));
+    GLCall(glBindVertexArray(vao));
+
     /* Data to give OpenGL */
     unsigned int buffer;
     GLCall(glGenBuffers(1, &buffer));
     /* Select which buffer will be drawn by OpenGL */
     GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
     /* Tell OpenGL what data is */
-    GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
 
     GLCall(glEnableVertexAttribArray(0));
+    /* Binds buffer to vao */
     GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
 
     unsigned int ibo;
@@ -170,6 +179,12 @@ int main(void)
     ASSERT(location != -1);
     GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
 
+    /* Unbind everything */
+    GLCall(glUseProgram(0));
+    GLCall(glBindVertexArray(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
     float r = 0.0f;
     float increment = 0.05f;
 
@@ -179,12 +194,23 @@ int main(void)
         /* Clear Screen */
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+        GLCall(glUseProgram(shader));
         GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
+        /*
+        * Unused du to specified vao
+         GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+         GLCall(glEnableVertexAttribArray(0));
+         GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
+         */
+
+        GLCall(glBindVertexArray(vao));
+        GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
-        if (r > 1.0f) 
+        if (r > 1.0f)
             increment = -0.05f;
-        else if (r < 0.0f) 
+        else if (r < 0.0f)
             increment = 0.05f;
 
         r += increment;
