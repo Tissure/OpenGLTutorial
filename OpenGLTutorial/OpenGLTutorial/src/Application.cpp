85573e5d6287e7bcba19ba184c8c2cf530c65047
Kyle <kyle.ng7512@gmail.com>
2023-02-23 16:58:39 -0800

    Complete part 14 - Buffer Layout Abstraction

--- OpenGLTutorial/OpenGLTutorial/src/Application.cpp
+++ OpenGLTutorial/OpenGLTutorial/src/Application.cpp
@@ -8,6 +8,7 @@
 
 #include "Renderer.h"
 
+#include "VertexArray.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
 
@@ -136,11 +137,12 @@ int main(void) {
             2,3,0
         };
 
-        unsigned int vao;
-        GLCall(glGenVertexArrays(1, &vao));
-        GLCall(glBindVertexArray(vao));
-
+        VertexArray va;
         VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        va.AddBuffer(vb, layout);
 
         GLCall(glEnableVertexAttribArray(0));
         GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
@@ -172,14 +174,7 @@ int main(void) {
             GLCall(glUseProgram(shader));
             GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 
-            /*
-            * Unused du to specified vao
-             GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
-             GLCall(glEnableVertexAttribArray(0));
-             GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0));
-             */
-
-            GLCall(glBindVertexArray(vao));
+            va.Bind();
             ib.Bind();
 
             GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
